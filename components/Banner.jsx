6a1df4d6976d222fe0dc4114b353d5b1c550a14c
Mohamed Elshamy <mo.mohamed45987@gmail.com>
2023-08-17 17:35:02 +0300

    Changed some styles

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -3,15 +3,15 @@ const Banner = () => {
     return (
         <section className="w-full p-[2vh] px-4 bg-banner text-banner-text flex justify-between">
             <ul className="flex gap-4">
-                <li><a href="#" >C21 GLOBAL</a></li>
-                <li><a href="#">LOG IN</a></li>
+                <li><a href="#" className="hover:text-white" >C21 GLOBAL</a></li>
+                <li><a href="#" className="hover:text-white">LOG IN</a></li>
             </ul>
             <ul className="flex gap-4">
-                <li><a href="#" >Franchise</a></li>
-                <li><a href="#">Company</a></li>
+                <li><a href="#" className="hover:text-white">Franchise</a></li>
+                <li><a href="#" className="hover:text-white">Company</a></li>
             </ul>
         </section>
     )
 }
- 
+
 export default Banner
--- components/ContactPage.jsx
+++ components/ContactPage.jsx
@@ -3,7 +3,7 @@ import ContactForm from "./ContactForm"
 
 const ContactPage = () => {
     return (
-        <section className="w-screen flex flex-col items-center" id="contact">
+        <section className="w-full flex flex-col items-center" id="contact">
             <Image src="/offices-img.png" width={1452} height={474} alt="Image of offices " className="w-full h-full" />
 
 
